191214bd258505367e8ff611f1fc611cde1f3bbf
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-12-03 19:29:10 +0530

    Update ApiTest.cpp

--- UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ApiTest.cpp
+++ UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ApiTest.cpp
@@ -1,11 +1,12 @@
 #include <gtest/gtest.h>
 #include "../test_library/CsvTester.h"
+#include "../utility/TestUtility.h"
 #include "TestMain.h"
 
 
 
-extern TestUtility s1;
-extern std::string filename;
+TestUtility s1;
+std::string filename;
     
 
 TEST(DdsAPI_Test, CSVReadType_1)
