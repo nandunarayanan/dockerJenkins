bf12059bddd5677dcaab51dcee82a8810ce50933
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-12-03 15:51:59 +0530

    Update ApiTest.cpp

--- UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ApiTest.cpp
+++ UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ApiTest.cpp
@@ -8,11 +8,28 @@ extern TestUtility s1;
 extern std::string filename;
     
 
-TEST(CSV_Test, CSV_Test)
+TEST(DdsAPI_Test, CSVReadType_1)
 {
 	{
 		filename = s1.getCsvFilePath("file1");
-		csvTestApiDDS(1);
-		std::cout<<"Inside TEST"<<endl;
+		s1.readCSV(filename);
+		for(int ab =0; ab<s1.words.size();ab++)
+		{
+			ASSERT_EQ(s1.words1[ab], s1.evaluvateTestcase(s1.words[ab]));
+		}
+		
+	}
+}
+
+TEST(DdsAPI_Test, CSVReadType_2)
+{
+	{
+		filename = s1.getCsvFilePath("file2");
+		s1.readCSV(filename);
+		for(int ab =0; ab<s1.words.size();ab++)
+		{
+			ASSERT_EQ(s1.words1[ab], s1.evaluvateTestcase(s1.words[ab]));
+		}
+		
 	}
 }
