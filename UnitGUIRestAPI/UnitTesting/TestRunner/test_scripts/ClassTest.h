5ef75b4dad931ca1aed6e75d9e91ddda25457f2d
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-12-04 00:05:27 +0530

    Update ClassTest.h

--- UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ClassTest.h
+++ UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ClassTest.h
@@ -1,6 +1,15 @@
 #ifndef CLASSTEST_H
 #define CLASSTEST_H
 
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+#include<string>
+#include<stdlib.h>
+#include<stdio.h>
+
+using namespace std;
+
 
 class Testpoints {
    static Testpoints *instance;
