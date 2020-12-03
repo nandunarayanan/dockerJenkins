55201a7f7611589a59b0ba26680ffc76723d5365
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-12-03 19:46:07 +0530

    Update ApiTest.cpp

--- UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ApiTest.cpp
+++ UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ApiTest.cpp
@@ -5,18 +5,18 @@
 
 
 
-TestUtility s1;
+TestUtility s;
 std::string filename;
     
 
 TEST(DdsAPI_Test, CSVReadType_1)
 {
 	{
-		filename = s1.getCsvFilePath("file1");
-		s1.readCSV(filename);
-		for(int ab =0; ab<s1.words.size();ab++)
+		filename = s.getCsvFilePath("file1");
+		s.readCSV(filename);
+		for(int ab =0; ab<s.words.size();ab++)
 		{
-			ASSERT_EQ(s1.words1[ab], s1.evaluvateTestcase(s1.words[ab]));
+			ASSERT_EQ(s.words1[ab], s.evaluvateTestcase(s.words[ab]));
 		}
 		
 	}
@@ -25,11 +25,11 @@ TEST(DdsAPI_Test, CSVReadType_1)
 TEST(DdsAPI_Test, CSVReadType_2)
 {
 	{
-		filename = s1.getCsvFilePath("file2");
-		s1.readCSV(filename);
-		for(int ab =0; ab<s1.words.size();ab++)
+		filename = s.getCsvFilePath("file2");
+		s.readCSV(filename);
+		for(int ab =0; ab<s.words.size();ab++)
 		{
-			ASSERT_EQ(s1.words1[ab], s1.evaluvateTestcase(s1.words[ab]));
+			ASSERT_EQ(s.words1[ab], s.evaluvateTestcase(s.words[ab]));
 		}
 		
 	}
