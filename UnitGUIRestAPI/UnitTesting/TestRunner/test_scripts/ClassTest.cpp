44fc8421704522b9bcb7704f160a768c177d0d61
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-12-06 09:45:43 +0530

    Update ClassTest.cpp

--- UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ClassTest.cpp
+++ UnitGUIRestAPI/UnitTesting/TestRunner/test_scripts/ClassTest.cpp
@@ -128,7 +128,8 @@ TEST_F(DoctorTest,getspec_csv)
 	CsvReader(filename);
 
 	for(int i=0;i<words1.size();i++)
-	{	
+	{
+		std::cout<<"words1 vector : "<<words1[i]<<endl;
 		s -> setD_spec(std::stoi(read_csv[i]));    
     		EXPECT_EQ(words1[i], d.getSpec());
  	}
